240b1940367dcad79f341c0fe5e62c2b17388b25
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-17 20:37:42 +0530

    factorial

--- src/main/java/com/itshaala/code/OddEven.java
+++ src/main/java/com/itshaala/code/OddEven.java
@@ -1,4 +1,15 @@
 package com.itshaala.code;
 
 public class OddEven {
+
+    public static void main(String[] args) {
+        int num = 8;
+
+        if(num%2==0){
+            System.out.println("even num it is");
+        }
+        else {
+            System.out.println("odd num it is");
+        }
+    }
 }
