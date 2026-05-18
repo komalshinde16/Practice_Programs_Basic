01894cbc480a18446be0c8078a5c4d623426fc96
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-18 18:08:07 +0530

    sum

--- src/main/java/com/itshaala/code/SumOfFirstNNumbers.java
+++ src/main/java/com/itshaala/code/SumOfFirstNNumbers.java
@@ -1,4 +1,14 @@
 package com.itshaala.code;
 
 public class SumOfFirstNNumbers {
+    public static void main(String[] args) {
+        int num = 8;
+        int sum = 0;
+
+        for(int i = 1; i<= num ; i++){
+            sum = sum + i;
+
+        }
+        System.out.println(sum);
+    }
 }
