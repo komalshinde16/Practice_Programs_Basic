782da6ef8771d83404a6e3d3a94284edb593e8fc
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-19 10:44:23 +0530

    p&n

--- src/main/java/com/itshaala/code/PositiveORNegative.java
+++ src/main/java/com/itshaala/code/PositiveORNegative.java
@@ -1,4 +1,17 @@
 package com.itshaala.code;
 
 public class PositiveORNegative {
+    public static void main(String[] args) {
+
+        int num = -8;
+
+        if(num>0){
+            System.out.println("positive number it is");
+        } else if (num < 0) {
+            System.out.println("negative number it is");
+
+        }else {
+            System.out.println("zero");
+        }
+    }
 }
