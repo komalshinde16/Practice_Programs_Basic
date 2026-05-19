3051202c754f87a35008ec8ce9bf267db7e9de2c
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-19 10:52:58 +0530

    3

--- src/main/java/com/itshaala/code/Largestof3Numbers.java
+++ src/main/java/com/itshaala/code/Largestof3Numbers.java
@@ -1,4 +1,19 @@
 package com.itshaala.code;
 
 public class Largestof3Numbers {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 777;
+        int c = 98;
+
+        if(a > b && a > c){
+            System.out.println("a is largest num");
+        } else if (b > a && b > c) {
+            System.out.println("b is largest num");
+
+        }else{
+            System.out.println("c is largest number");
+        }
+    }
 }
