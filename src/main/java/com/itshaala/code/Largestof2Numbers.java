9bbf65b61dfc174300c81f7c5e5321613bed87de
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-19 10:47:27 +0530

    2

---
+++ src/main/java/com/itshaala/code/Largestof2Numbers.java
@@ -0,0 +1,17 @@
+package com.itshaala.code;
+
+public class Largestof2Numbers {
+
+    public static void main(String[] args) {
+
+        int a = 89;
+        int b = 87;
+
+        if(a>b){
+            System.out.println("a is largest num");
+        }else {
+            System.out.println("b is largest num");
+
+        }
+    }
+}
