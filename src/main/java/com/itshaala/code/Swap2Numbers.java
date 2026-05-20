f8fb081a4dcd5665823ba7cde65bfa20dfb4085b
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-20 15:02:45 +0530

    swapping

--- src/main/java/com/itshaala/code/Swap2Numbers.java
+++ src/main/java/com/itshaala/code/Swap2Numbers.java
@@ -1,4 +1,22 @@
 package com.itshaala.code;
 
 public class Swap2Numbers {
+    public static void main(String[] args) {
+        int a =90;
+        int b=89;
+        int temp;
+
+        System.out.println("before swapping:");
+        System.out.println(a);
+        System.out.println(b);
+
+        temp = a;
+        a=b;
+        b = temp;
+
+        System.out.println("after swapping:");
+        System.out.println(a);
+        System.out.println(b);
+
+    }
 }
