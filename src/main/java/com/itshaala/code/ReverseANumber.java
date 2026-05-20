f6f89e5ccab7256b48f66d8b6c746e3d337a06af
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-20 15:11:54 +0530

    reverse num

--- src/main/java/com/itshaala/code/ReverseANumber.java
+++ src/main/java/com/itshaala/code/ReverseANumber.java
@@ -1,4 +1,16 @@
 package com.itshaala.code;
 
 public class ReverseANumber {
+
+    public static void main(String[] args) {
+        int num = 1234;
+        int rev = 0;
+
+        while(num!=0){
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+        System.out.println(rev);
+    }
 }
