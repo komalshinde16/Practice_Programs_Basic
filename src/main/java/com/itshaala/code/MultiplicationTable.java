b1f7d0c264304a9500db45e78192cda4b51d00b0
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-21 09:53:20 +0530

    table

--- src/main/java/com/itshaala/code/MultiplicationTable.java
+++ src/main/java/com/itshaala/code/MultiplicationTable.java
@@ -1,4 +1,12 @@
 package com.itshaala.code;
 
 public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        int num = 4;
+
+        for(int i = 1; i<=10; i++){
+            System.out.println(num + "x" + i +  "=" + (num*i));
+        }
+    }
 }
