be8df5f0fb029e4fd95ad403d207e144d0d43c40
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-21 09:59:26 +0530

    prime num

--- src/main/java/com/itshaala/code/PrimeNumber.java
+++ src/main/java/com/itshaala/code/PrimeNumber.java
@@ -1,4 +1,23 @@
 package com.itshaala.code;
 
 public class PrimeNumber {
+    public static void main(String[] args) {
+
+        int num = 9;
+        boolean isPrime = false;
+
+        if(num<=1 ) isPrime = true;
+
+        for(int i =2; i<= num/2; i++){
+            if(num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println("Prime number it is");
+        }else {
+            System.out.println("not a prime number");
+        }
+    }
 }
