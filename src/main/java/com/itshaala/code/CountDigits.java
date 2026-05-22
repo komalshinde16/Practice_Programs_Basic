b4d8d539be9092e29bdc8a1617e770fcdded8256
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-22 12:18:35 +0530

    count digits

--- src/main/java/com/itshaala/code/CountDigits.java
+++ src/main/java/com/itshaala/code/CountDigits.java
@@ -1,5 +1,10 @@
 package com.itshaala.code;
 
 public class CountDigits {
+    public static void main(String[] args) {
+        int num = 867744;
+        int count = String.valueOf(num).length();
+        System.out.println(count);
+    }
 
 }
