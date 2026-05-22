627746129e0d323d1172749b829a4c8d6119c677
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-22 12:24:58 +0530

    d2b

--- src/main/java/com/itshaala/code/Decimal2Binary.java
+++ src/main/java/com/itshaala/code/Decimal2Binary.java
@@ -1,4 +1,13 @@
 package com.itshaala.code;
 
+
+
 public class Decimal2Binary {
+    public static void main(String[] args) {
+        int num = 34;
+
+        String binary = Integer.toBinaryString(num);
+        System.out.println(binary);
+
+    }
 }
