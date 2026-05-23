763442c341bf640657e9be82f3f77c2169092ab4
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-23 14:10:16 +0530

    number

--- src/main/java/com/itshaala/code/PalindromeNumber.java
+++ src/main/java/com/itshaala/code/PalindromeNumber.java
@@ -1,4 +1,28 @@
 package com.itshaala.code;
 
+import java.util.Scanner;
+
 public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num = scanner.nextInt();
+
+        int temp = num;
+        int rev = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+
+        if (temp == rev)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not a Palindrome Number");
+    }
+
+
 }
