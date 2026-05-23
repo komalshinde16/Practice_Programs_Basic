7f03352059acea7c55c66da1702ba9af81b2b79a
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-23 14:06:38 +0530

    sum

--- src/main/java/com/itshaala/code/SumofDigits.java
+++ src/main/java/com/itshaala/code/SumofDigits.java
@@ -1,4 +1,15 @@
 package com.itshaala.code;
 
 public class SumofDigits {
+
+    public static void main(String[] args) {
+        int n = 2;
+
+        int sum = 0;
+
+        for(int i = 0; i<=n ; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
 }
