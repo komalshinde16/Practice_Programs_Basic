a1b1e6e25f93927cdbab6acdedabf4056ae27f25
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-24 10:25:05 +0530

    dec2oct

--- src/main/java/com/itshaala/code/Decimal2Octal.java
+++ src/main/java/com/itshaala/code/Decimal2Octal.java
@@ -1,4 +1,11 @@
 package com.itshaala.code;
 
 public class Decimal2Octal {
+
+    public static void main(String[] args) {
+        int num = 34;
+
+        String octal = Integer.toOctalString(num);
+        System.out.println(octal);
+    }
 }
