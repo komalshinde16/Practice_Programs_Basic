d4d4c9dd2d1dd4cff1023c0b3c94d52b940d2236
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-25 14:31:59 +0530

    print arrays

--- src/main/java/com/itshaala/Arrays/PrintArrays.java
+++ src/main/java/com/itshaala/Arrays/PrintArrays.java
@@ -1,4 +1,14 @@
 package com.itshaala.Arrays;
 
 public class PrintArrays {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+        for(int i = 0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+
+
+    }
+
 }
