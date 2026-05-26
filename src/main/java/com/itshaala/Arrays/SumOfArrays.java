7dace03c1615079772a1186d2bab66b045d7b908
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-26 10:07:22 +0530

    sum

--- src/main/java/com/itshaala/Arrays/SumOfArrays.java
+++ src/main/java/com/itshaala/Arrays/SumOfArrays.java
@@ -5,5 +5,10 @@ public class SumOfArrays {
 
 
         int [] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int sum = 0;
+
+        for(int i=0; i< array.length; i++){
+            sum = sum + array[i];
+        }
     }
 }
