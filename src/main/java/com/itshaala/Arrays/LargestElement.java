05259967d8bc82456b43e0c4a50141ccb342034b
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-27 14:10:22 +0530

    max

--- src/main/java/com/itshaala/Arrays/LargestElement.java
+++ src/main/java/com/itshaala/Arrays/LargestElement.java
@@ -4,5 +4,12 @@ public class LargestElement {
     public static void main(String[] args) {
 
         int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int max = array[0];
+
+        for(int i=0; i< array.length; i++){
+            if (array[i]> max){
+                max = array[i];
+            }
+        }
     }
 }
