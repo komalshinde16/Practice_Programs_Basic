3f2bf184363ac34b5d00dec016469b93daff3c95
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-28 13:31:17 +0530

    max

--- src/main/java/com/itshaala/Arrays/MaximumElement.java
+++ src/main/java/com/itshaala/Arrays/MaximumElement.java
@@ -5,6 +5,12 @@ public class MaximumElement {
         int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
         int max = 0;
 
+        for(int i=0; i< array.length; i++){
+            if(array[i]> max){
+                max = array[i];
+            }
+        }
+
 
     }
 }
