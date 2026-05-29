44534626be4fae326cdd8cfa5154ddc9da774653
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-29 13:50:57 +0530

    min

--- src/main/java/com/itshaala/Arrays/SmallestElement.java
+++ src/main/java/com/itshaala/Arrays/SmallestElement.java
@@ -5,7 +5,11 @@ public class SmallestElement {
         int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
         int min = 0;
 
-
+     for(int i=0; i< array.length; i++){
+         if(array[i]<min){
+             min = array[i];
+         }
+     }
 
 
 
