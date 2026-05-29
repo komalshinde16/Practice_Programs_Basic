4a9905dcc490275a2537d7ac832cef59fc4b2d42
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-29 13:49:49 +0530

    min

--- src/main/java/com/itshaala/Arrays/SmallestElement.java
+++ src/main/java/com/itshaala/Arrays/SmallestElement.java
@@ -3,5 +3,11 @@ package com.itshaala.Arrays;
 public class SmallestElement {
     public static void main(String[] args) {
         int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int min = 0;
+
+
+
+
+
     }
 }
