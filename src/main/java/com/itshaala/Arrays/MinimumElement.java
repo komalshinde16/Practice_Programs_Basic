c1daee9acf0c00e8de88fa34cc6e668f7c52a557
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-30 14:29:09 +0530

    i

--- src/main/java/com/itshaala/Arrays/MinimumElement.java
+++ src/main/java/com/itshaala/Arrays/MinimumElement.java
@@ -3,5 +3,12 @@ package com.itshaala.Arrays;
 public class MinimumElement {
     public static void main(String[] args) {
         int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int min = array[0];
+
+        for(int i=0; i< array.length; i++){
+            if(array[i]<min){
+                min = array[i];
+            }
+        }
     }
 }
