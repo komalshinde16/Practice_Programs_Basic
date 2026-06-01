bb82d996006824b8952bd12d74da901b1423201a
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-01 12:34:23 +0530

    reverse an array

--- src/main/java/com/itshaala/Arrays/Reverse02.java
+++ src/main/java/com/itshaala/Arrays/Reverse02.java
@@ -2,8 +2,12 @@ package com.itshaala.Arrays;
 
 public class Reverse02 {
     public static void main(String[] args) {
+
         int [] array = {21,52,458,45,36,94,12};
 
-        for(int i= array.length-1; i>=0; i--)
+        for(int i= array.length-1; i>=0; i--){
+
+            System.out.println(array[i]+ " ");
+        }
     }
 }
