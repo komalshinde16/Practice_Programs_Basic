053036186fe1cc6b3cb8c25ae03fc2bc0b5ea8f5
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-02 08:50:48 +0530

    sum

--- src/main/java/com/itshaala/Arrays/Sum02.java
+++ src/main/java/com/itshaala/Arrays/Sum02.java
@@ -3,5 +3,12 @@ package com.itshaala.Arrays;
 public class Sum02 {
     public static void main(String[] args) {
         int [] array = {1,2,3,32,1,24};
+        int sum = 0;
+
+        for(int i=0; i< array.length; i++){
+            sum = sum + array[i];
+
+        }
+
     }
 }
