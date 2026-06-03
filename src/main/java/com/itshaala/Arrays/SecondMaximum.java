5922046ec069ba688acb6731f6b283f3fd1d79b1
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-03 12:20:21 +0530

    2

--- src/main/java/com/itshaala/Arrays/SecondMaximum.java
+++ src/main/java/com/itshaala/Arrays/SecondMaximum.java
@@ -4,5 +4,8 @@ public class SecondMaximum {
     public static void main(String[] args) {
 
         int [] array = {1,2,3,32,1,24};
+
+        int max = array[0];
+        int secondMax = array[0];
     }
 }
