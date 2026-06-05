f1667b124e8aae5d34959ae3ce582618665e0975
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-05 14:21:46 +0530

    min

--- src/main/java/com/itshaala/Arrays/SecondMinimum.java
+++ src/main/java/com/itshaala/Arrays/SecondMinimum.java
@@ -5,8 +5,19 @@ public class SecondMinimum {
 
         int [ ] array = {12,87,56,96,32,4,8755,44};
 
-        int max = array[0];
-        int secondMax = array[0];
+        int min = array[0];
+        int secondMin = array[0];
+
+
+        for (int i = 1; i < array.length; i++) {
+
+            if (array[i] < min) {
+                secondMin = min;
+                min = array[i];
+            } else if (array[i] < secondMin && array[i] != min) {
+                secondMin = array[i];
+            }
+        }
 
 
     }
