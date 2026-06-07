7b92cb7843f2aabb062f7ea572a7df3d6d6fa4f0
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-07 12:50:12 +0530

    dup

--- src/main/java/com/itshaala/Arrays/DuplicatesRemove.java
+++ src/main/java/com/itshaala/Arrays/DuplicatesRemove.java
@@ -1,9 +1,17 @@
 package com.itshaala.Arrays;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class DuplicatesRemove {
     public static void main(String[] args) {
 
         int[] array = {1,2,3,4,5,6,1,2,3,4};
+
+        Set<Integer> set = new LinkedHashSet<>();
+        for(int num:array){
+            set.add(num);
+        }
 
     }
 }
