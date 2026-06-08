21f8801e12c4b2e249db81495045fda78d28762b
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-08 11:10:09 +0530

    count

--- src/main/java/com/itshaala/Arrays/CountEvenOddInAnArray.java
+++ src/main/java/com/itshaala/Arrays/CountEvenOddInAnArray.java
@@ -1,4 +1,11 @@
 package com.itshaala.Arrays;
 
 public class CountEvenOddInAnArray {
+    public static void main(String[] args) {
+
+        int[] array = {10, 15, 20, 25, 30, 35, 40};
+
+        int evenCount = 0;
+        int oddCount = 0;
+    }
 }
