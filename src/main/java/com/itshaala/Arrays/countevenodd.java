b436f1ef982eb7bdde43391cfe1de7881c60856b
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-09 10:15:58 +0530

    count even

--- src/main/java/com/itshaala/Arrays/countevenodd.java
+++ src/main/java/com/itshaala/Arrays/countevenodd.java
@@ -6,6 +6,18 @@ public class countevenodd {
 
         int[] array = {10, 15, 20, 25, 30, 35, 40};
 
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
 
     }
 }
