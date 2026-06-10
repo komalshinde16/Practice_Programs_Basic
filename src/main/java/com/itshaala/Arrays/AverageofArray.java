6713caf9cf5aaf63b6c72473c8f34505f3a33226
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-10 19:54:24 +0530

    avg

--- src/main/java/com/itshaala/Arrays/AverageofArray.java
+++ src/main/java/com/itshaala/Arrays/AverageofArray.java
@@ -6,4 +6,12 @@ public class AverageofArray {
 
         int[] array = {10, 20, 30, 40, 50};
 
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
     }
+}
