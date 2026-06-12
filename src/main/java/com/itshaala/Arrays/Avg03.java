aaad5e1b4913d53495c32d55cc6aacb3c6e4319d
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-12 20:54:55 +0530

    avg

--- src/main/java/com/itshaala/Arrays/Avg03.java
+++ src/main/java/com/itshaala/Arrays/Avg03.java
@@ -3,5 +3,11 @@ package com.itshaala.Arrays;
 public class Avg03 {
     public static void main(String[] args) {
 
+
+        int[] array = {10, 20, 30, 40, 50};
+
+
+        int sum = 0;
+
     }
 }
