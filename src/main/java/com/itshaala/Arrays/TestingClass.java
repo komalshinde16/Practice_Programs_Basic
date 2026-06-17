9913f34ae592a0d13c86dd216cb63a43dc160f59
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-17 09:21:52 +0530

    even

--- src/main/java/com/itshaala/Arrays/TestingClass.java
+++ src/main/java/com/itshaala/Arrays/TestingClass.java
@@ -3,5 +3,9 @@ package com.itshaala.Arrays;
 public class TestingClass {
     public static void main(String[] args) {
         int num = 76;
+
+        if (num%2==0){
+            System.out.println("even number it is");
+        }
     }
 }
