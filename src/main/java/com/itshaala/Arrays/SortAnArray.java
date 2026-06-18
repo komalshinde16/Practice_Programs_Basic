b6c61534fd32d3833985bebd94ccd0d52a98dae2
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-18 07:37:12 +0530

    sort

--- src/main/java/com/itshaala/Arrays/SortAnArray.java
+++ src/main/java/com/itshaala/Arrays/SortAnArray.java
@@ -1,7 +1,11 @@
 package com.itshaala.Arrays;
 
+import java.util.Arrays;
+
 public class SortAnArray {
     public static void main(String[] args){
         int [] array = {7,2,4,5,2,1,4,5,5,1,2,};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
