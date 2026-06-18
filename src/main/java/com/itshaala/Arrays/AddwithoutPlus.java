c3c0a2f70c4cf3af8d355a2ffa2c32ade3a94949
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-18 07:43:18 +0530

    addition without using + operator

---
+++ src/main/java/com/itshaala/Arrays/AddwithoutPlus.java
@@ -0,0 +1,11 @@
+package com.itshaala.Arrays;
+
+public class AddwithoutPlus {
+    public static void main(String[] args) {
+        int a= 3;
+        int b = 2;
+
+        int sum = -(-a-b);
+        System.out.println(sum);
+    }
+}
