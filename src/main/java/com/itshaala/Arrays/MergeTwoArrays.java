10334fc44733c26c02abe751ee236614cd5985a6
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-19 08:39:47 +0530

    merge

--- src/main/java/com/itshaala/Arrays/MergeTwoArrays.java
+++ src/main/java/com/itshaala/Arrays/MergeTwoArrays.java
@@ -3,5 +3,9 @@ package com.itshaala.Arrays;
 public class MergeTwoArrays {
     public static void main(String[] args) {
 
+        int [] array1= {1,2,3};
+        int [] array2 = {4,5,6};
+
+
     }
 }
