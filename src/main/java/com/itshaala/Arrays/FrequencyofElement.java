cf5481e2df9ccb4587c063fef5336209de847be6
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-20 10:55:29 +0530

    f

--- src/main/java/com/itshaala/Arrays/FrequencyofElement.java
+++ src/main/java/com/itshaala/Arrays/FrequencyofElement.java
@@ -3,5 +3,14 @@ package com.itshaala.Arrays;
 public class FrequencyofElement {
     public static void main(String[] args) {
         int [] array = {1, 2, 2, 3, 1, 4, 2};
+
+        boolean[] visited = new boolean[array.length];
+
+        for (int i = 0; i < array.length ; i++) {
+
+            if (visited[i])
+                continue;
+
+        }
     }
 }
